e4fa4131ec03712f3f94db718e33852e91e7a54e
dennispetrillo <7047278+dennispetrillo@users.noreply.github.com>
2018-08-12 08:05:59 -0400

    updated version

--- habitat/plan.sh
+++ habitat/plan.sh
@@ -1,6 +1,6 @@
 pkg_origin=habitat-sandbox
 pkg_name=np-mongo
-pkg_version=3.2.9
+pkg_version=4.0
 pkg_description="Mongodb for National-Parks app"
 pkg_maintainer="echohack"
 pkg_license=('AGPL-3.0')
